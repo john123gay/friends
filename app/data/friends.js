7fd6c9019b8a34f57697a6d5dad630dd2dcbd991
john <one23four5ate9@yahoo.com>
2018-12-08 20:05:27 -0500

    swagger

---
+++ app/data/friends.js
@@ -0,0 +1,8 @@
+var friendsArray = [
+    {
+        name:"Gomez",
+        photo:"https://www.trbimg.com/img-532f3566/turbine/bs-ae-addams-family-astin-20120309",
+        scores: [4,3,4,3,2,1,2,3,5,3]
+    }
+];
+module.exports = friendsArray;
